39c8ee72b3747542441565b45e2ffbef087b4052
Terry <terry5@foxmail.com>
2025-10-03 18:20:36 +0800

    feat: 基础 props 添加 autoSelectFirst prop

--- src/core/options.js
+++ src/core/options.js
@@ -1,13 +1,21 @@
 import { mergeProps } from 'vue'
 
 import { CN } from '../language'
-
+/**
+ * 基础 props
+ *
+ * 作用于：Select、Group、Column、Text
+ *
+ * @param {object} props
+ * @returns
+ */
 export function mergeBaseProps (props) {
   return mergeProps({
     city: { type: Boolean, default: true },
     area: { type: Boolean, default: true },
     town: { type: Boolean, default: true },
     language: { type: String, default: CN },
+    autoSelectFirst: { type: Boolean, default: true },
     modelValue: { type: Object, default: undefined }
   }, props)
 }
